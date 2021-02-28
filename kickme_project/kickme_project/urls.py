fe85c5c10e37498258c5ca4e85943d3b99a7c4ec
Roman Cheban <roman.cheburan@gmail.com>
2021-02-28 20:11:56 +0200

    created 'home_view' view created '' and 'kick/<int:kick_id>' url patterns

--- kickme_project/kickme_project/urls.py
+++ kickme_project/kickme_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from kicks.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view),
+    path('kick/<int:kick_id>', home_view),
 ]
--- kickme_project/kicks/views.py
+++ kickme_project/kicks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Kick
 
-# Create your views here.
+
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse("<h1>Hello World</h1>")
